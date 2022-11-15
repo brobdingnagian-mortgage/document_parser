782c176f7ecc2fccfa3ddd6108323578e63c8e4f
brobdingnagian-mortgage <118125834+brobdingnagian-mortgage@users.noreply.github.com>
2022-11-14 18:07:42 -0600

    Removing secret
    Hey Bia, make sure we don't include production API keys in our public repo.

--- Scripts/brob-mort-api.py
+++ Scripts/brob-mort-api.py
@@ -3,6 +3,6 @@
 import requests
 import json
 
-customer_data = requests.get('https://brobdingnagian-mortgage.com/api/v1/728a950c5c3ac65df139e59b9df5bd27/loans/all')
+customer_data = requests.get('https://brobdingnagian-mortgage.com/api/v1/{APIKEY}/loans/all')
 customer_json = json.loads(customer_data.content)
 print(json.dumps(customer_json, indent=2))
